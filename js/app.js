9c2bf335421a548ed0ea047f4c44e10cf97fe74c
Pawel <p.p.wronski@gmail.com>
2016-03-08 22:43:48 +0100

    summery block, globe

--- js/app.js
+++ js/app.js
@@ -3,4 +3,11 @@
  */
 console.log('test');
 
-
+//$("img.lazy").lazyload({
+//    effect : "fadeIn"
+//});
+$("img").unveil(200, function() {
+    $(this).load(function() {
+        this.style.opacity = 1;
+    });
+});
