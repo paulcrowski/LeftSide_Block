a0040b57eb3747f2ece4c5cf951a5f1923408597
Pawel <p.p.wronski@gmail.com>
2016-03-13 22:50:00 +0100

    readme boxes, and inputs

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
 /**
  * Created by Pol on 2015-12-31.
  */
+
+
+// no transition on load on explorer
+
+$(document).ready(function () {
+    $('body').removeClass('notransition')
+});
+
 console.log('test');
 
 //$("img.lazy").lazyload({
@@ -10,4 +18,24 @@ $("img").unveil(200, function() {
     $(this).load(function() {
         this.style.opacity = 1;
     });
+});
+
+// checkboxes ##################### glyphicons
+
+$(document).ready(function () {
+    $("[type='checkbox']").on('change', function(){
+        if ($(this).prop('checked')) {
+            $(this).prev()
+                .removeClass('glyphicon-unchecked')
+                .addClass('glyphicon-ok-circle');
+        }
+        else {
+            $(this).prev()
+                .removeClass('glyphicon-ok-circle')
+                .addClass('glyphicon-unchecked');
+        }
+    });
+    $(this).prev()
+        .removeClass('glyphicon-ok-circle')
+        .addClass('glyphicon-unchecked');
 });
